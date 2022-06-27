d0af7aba3fbe9b665b91113db4498d27189442e2
Vidya Reddy <59590642+Vidya2606@users.noreply.github.com>
2022-06-27 11:45:55 -0700

    Vidya reddy prettier (#31)
    * upgraded to Node16
    
    * Enforce Prettier
    
    * code fix
    
    * Code fix
    
    * code fix
    
    * jest version change
    
    Co-authored-by: Vidya Reddy <vidyareddy@microsoft.com>

--- src/kubeval/kubeval.ts
+++ src/kubeval/kubeval.ts
@@ -1,65 +1,65 @@
-import * as os from "os";
-import * as path from "path";
-import * as fs from "fs";
-import * as toolCache from "@actions/tool-cache";
-import * as core from "@actions/core";
-import * as io from "@actions/io";
-import { ToolRunner } from "@actions/exec/lib/toolrunner";
-import { getExecutableExtension } from "../utils";
+import * as os from 'os'
+import * as path from 'path'
+import * as fs from 'fs'
+import * as toolCache from '@actions/tool-cache'
+import * as core from '@actions/core'
+import * as io from '@actions/io'
+import {ToolRunner} from '@actions/exec/lib/toolrunner'
+import {getExecutableExtension} from '../utils'
 
-const TOOL_NAME = "kubeval";
+const TOOL_NAME = 'kubeval'
 
 export async function kubevalLint(manifests: string[]) {
-  const toolPath =
-    (await io.which(TOOL_NAME, false)) || (await downloadKubeval());
+   const toolPath =
+      (await io.which(TOOL_NAME, false)) || (await downloadKubeval())
 
-  for (const manifest of manifests) {
-    const toolRunner = new ToolRunner(toolPath, [manifest]);
-    const code = await toolRunner.exec();
+   for (const manifest of manifests) {
+      const toolRunner = new ToolRunner(toolPath, [manifest])
+      const code = await toolRunner.exec()
 
-    if (code != 0) {
-      throw Error("Your manifest has errors");
-    }
-  }
+      if (code != 0) {
+         throw Error('Your manifest has errors')
+      }
+   }
 }
 
 export async function downloadKubeval(): Promise<string> {
-  const downloadPath = await toolCache.downloadTool(getKubevalDownloadUrl());
+   const downloadPath = await toolCache.downloadTool(getKubevalDownloadUrl())
 
-  // extract from download
-  let extractedPath;
-  switch (os.type()) {
-    case "Linux":
-    case "Darwin":
-      const newPath = path.join(path.dirname(downloadPath), "tool.tar.gz");
-      await io.cp(downloadPath, newPath);
-      extractedPath = await toolCache.extractTar(newPath);
-      break;
-    case "Windows_NT":
-    default:
-      extractedPath = await toolCache.extractZip(downloadPath);
-  }
+   // extract from download
+   let extractedPath
+   switch (os.type()) {
+      case 'Linux':
+      case 'Darwin':
+         const newPath = path.join(path.dirname(downloadPath), 'tool.tar.gz')
+         await io.cp(downloadPath, newPath)
+         extractedPath = await toolCache.extractTar(newPath)
+         break
+      case 'Windows_NT':
+      default:
+         extractedPath = await toolCache.extractZip(downloadPath)
+   }
 
-  // get and make executable
-  const kubevalPath = path.join(
-    extractedPath,
-    TOOL_NAME + getExecutableExtension()
-  );
-  fs.chmodSync(kubevalPath, "777");
-  core.addPath(extractedPath);
-  return kubevalPath;
+   // get and make executable
+   const kubevalPath = path.join(
+      extractedPath,
+      TOOL_NAME + getExecutableExtension()
+   )
+   fs.chmodSync(kubevalPath, '777')
+   core.addPath(extractedPath)
+   return kubevalPath
 }
 
 export function getKubevalDownloadUrl(): string {
-  switch (os.type()) {
-    case "Linux":
-      return "https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-linux-amd64.tar.gz";
+   switch (os.type()) {
+      case 'Linux':
+         return 'https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-linux-amd64.tar.gz'
 
-    case "Darwin":
-      return "https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-darwin-amd64.tar.gz";
+      case 'Darwin':
+         return 'https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-darwin-amd64.tar.gz'
 
-    case "Windows_NT":
-    default:
-      return "https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-windows-amd64.zip";
-  }
+      case 'Windows_NT':
+      default:
+         return 'https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-windows-amd64.zip'
+   }
 }
--- src/run.ts
+++ src/run.ts
@@ -1,21 +1,21 @@
-import * as core from "@actions/core";
+import * as core from '@actions/core'
 
-import { kubevalLint } from "./kubeval/kubeval";
-import { kubectlLint } from "./kubectl/kubectl";
+import {kubevalLint} from './kubeval/kubeval'
+import {kubectlLint} from './kubectl/kubectl'
 
 export async function kubeval() {
-  // get inputs
-  const type = core.getInput("lintType", { required: true });
-  const manifestsInput = core.getInput("manifests", { required: true });
-  const manifests = manifestsInput.split("\n");
+   // get inputs
+   const type = core.getInput('lintType', {required: true})
+   const manifestsInput = core.getInput('manifests', {required: true})
+   const manifests = manifestsInput.split('\n')
 
-  if (type.toLocaleLowerCase() === "dryrun") {
-    const namespace =
-      core.getInput("namespace", { required: false }) || "default";
-    await kubectlLint(manifests, namespace);
-  } else {
-    await kubevalLint(manifests);
-  }
+   if (type.toLocaleLowerCase() === 'dryrun') {
+      const namespace =
+         core.getInput('namespace', {required: false}) || 'default'
+      await kubectlLint(manifests, namespace)
+   } else {
+      await kubevalLint(manifests)
+   }
 }
 
-kubeval().catch(core.setFailed);
+kubeval().catch(core.setFailed)
